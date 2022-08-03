ce59610f213739216bf1f83fef6d0259920e8da8
joyboi616 <200455316@student.georgianc.on.ca>
2022-08-03 13:50:04 -0400

    updated files

--- movie-franchises/src/components/Navbar.js
+++ movie-franchises/src/components/Navbar.js
@@ -6,14 +6,13 @@ const navbar = () => {
     <header>
       <nav className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
-          <NavLink className="navbar-brand" to="/">CRUD APP</NavLink>
+          <NavLink className="navbar-brand" to="/">Personal Favorites</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
               </li>
               {/* <li class="nav-item">
                 <a class="nav-link" href="#">Link</a>
@@ -23,10 +22,13 @@ const navbar = () => {
                   Dropdown
                 </NavLink>
                 <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="/">Action</a></li>
-                  <li><a className="dropdown-item" href="/">Another action</a></li>
+                  <li><a className="dropdown-item" href="/">Comics</a></li>
                   <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="/">Something else here</a></li>
+                  <li><a className="dropdown-item" href="/">Movies</a></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item" href="/">Music</a></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item" href="/">Television</a></li>
                 </ul>
               </li>
               {/* <li class="nav-item">
